47c1ba1aa91693632911415109d5cf46b60132dc
Benton Chan <chan.benton+test@gmail.com>
2018-12-02 12:03:45 -0500

    Practice w/ conditional queries for route

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Route w/ params
 Route::get('/test/{id}/{name}', function ($id,$name) {
 	return "ID Number: " . $id . ", from " . $name;
 });
+
+// Optional params
+Route::get('user/{name?}', function($name = null) {
+    return $name;
+});
+
+Route::get('/admin/posts/example', array('as'=>'admin.home', function () {
+	$url = route('admin.home');
+	return "this url is ". $url;
+}));
+
