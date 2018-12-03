202bd638ae66ad4e9a4d202df301098685428c8d
Benton Chan <chan.benton+test@gmail.com>
2018-12-02 22:43:07 -0500

    added blade templating

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Docs</title>
+</head>
+<body>
+
+<div>
+    @yield('content')
+</div>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -8,36 +8,12 @@
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
+| Reference: https://laravel.com/docs/4.2/routing#basic-routing
 |
 */
-
-// Restricts all usages of 'id' to numbers only
-Route::pattern('id', '[0-9]+');
-
-
 Route::get('/', function () {
-    return view('welcome');
+	return 'This is the home page!';
 });
+// Route::resource('post', 'PostController');
 
-// Route w/ params
-Route::get('/test/{id}/{name}', function ($id,$name) {
-	return "ID Number: " . $id . ", from " . $name;
-});
-
-// Optional params, enables /user to work
-Route::get('user/{name?}', function($name = null) {
-    return $name;
-});
-// // '/user' would not work below standalone
-// Route::get('user/{name}', function($name) {
-//     return $name;
-// });
-
-
-
-// Aliasing URLs
-Route::get('/admin/posts/example', array('as'=>'page.home', function () {
-	$url = route('page.home');
-	return "this url is ". $url;
-}));
-
+Route::get('/post', 'PostController@index');
